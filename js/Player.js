f3672a1229e64d33a670fc1eaa851b9a1c222278
C Dhivya Bharathi <bharathi.chellakumar@gmail.com>
2020-08-21 17:54:12 +0530

    js/Player.js

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -18,6 +19,7 @@ class Player {
     });
   }
 
+
   update(){
     var playerIndex = "players/player" + this.index;
     database.ref(playerIndex).set({
@@ -30,6 +32,19 @@ class Player {
     var playerInfoRef = database.ref('players');
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
+    })
+  }
+
+  getCarsAtEnd(){
+    database.ref('CarsAtEnd').on("value",(data)=>{
+      this.rank = data.val();
+    })
+
+  }
+
+  static updateCarsAtEnd(rank){
+    database.ref('/').update({
+      CarsAtEnd:rank
     })
   }
 }
